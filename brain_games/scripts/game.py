11ec6ac57237eb6a8f00afdc271e8d379ebf14aa
Andrey Vershinin <adw006@ya.ru>
2020-09-04 21:56:55 +0300

    refactor start_game function

--- brain_games/scripts/game.py
+++ brain_games/scripts/game.py
@@ -2,6 +2,28 @@
 
 import prompt
 
+COUNT = 3
+
+
+def ask_questions(get_question, count):
+    try_count = count
+
+    while try_count:
+        question, correct_answer = get_question()
+
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+
+        if answer == correct_answer:
+            print('Correct!')
+            try_count -= 1
+        else:
+            print(f"'{answer}' is wrong answer ;(.", end=' ')
+            print(f"Correct answer was '{correct_answer}'.")
+            return False
+
+    return True
+
 
 def start_game(game=None):
     if game is None:
@@ -14,23 +36,9 @@ def start_game(game=None):
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!\n')
 
-    try_count = 3
+    is_winner = ask_questions(game.get_question, COUNT)
 
-    while try_count:
-        question, correct_answer = game.get_question()
-
-        print(f'Question: {question}')
-        answer = prompt.string('Your answer: ')
-
-        if answer == correct_answer:
-            print('Correct!')
-            try_count -= 1
-        else:
-            print(f"'{answer}' is wrong answer ;(.", end=' ')
-            print(f"Correct answer was '{correct_answer}'.")
-            break
-
-    if try_count:
-        print(f"Let's try again, {user_name}!")
-    else:
+    if is_winner:
         print(f'Congratulations, {user_name}!')
+    else:
+        print(f"Let's try again, {user_name}!")
